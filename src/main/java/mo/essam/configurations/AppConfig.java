8f5cdf78e2d41056a5506892299a8c9b6d5042cc
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-20 13:58:01 +0200

    Now Code will run without error because we make sumsung qualify to be beans by using @Bean Annotation

--- src/main/java/mo/essam/configurations/AppConfig.java
+++ src/main/java/mo/essam/configurations/AppConfig.java
@@ -1,11 +1,13 @@
 package mo.essam.configurations;
 
 import mo.essam.models.Sumsung;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    @Bean
     public Sumsung getSumsung(){
         return new Sumsung();
     }
