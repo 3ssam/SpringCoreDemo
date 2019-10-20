226073beddd663f5e7859c87877f6c5ef5d283b0
Mohamed Essam <ana3ssam2017@gmail.com>
2019-10-20 14:13:58 +0200

    @Component Work and get beans in Configuration Class by Using @ComponentScan Annotation to search about all available Component can to inject them as beans in this class

--- src/main/java/mo/essam/configurations/AppConfig.java
+++ src/main/java/mo/essam/configurations/AppConfig.java
@@ -4,9 +4,11 @@ import mo.essam.interfaces.MobileProcessor;
 import mo.essam.models.SnabDragon;
 import mo.essam.models.Sumsung;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "mo.essam")
 public class AppConfig {
 
 }
